e456b5812846b35fcc18a33da125c2b0b7afd471
Oran C <oranbusiness@gmail.com>
2018-01-05 04:27:56 -0800

    Created a signup page, and a controller for it

--- routes.py
+++ routes.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from models import db
+from form import SignupForm
 
 app = Flask(__name__)
 
+# connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+db.init_app(app)
+
+
+app.secret_key = "develompent-key"
+
+
+# render the homepage
 @app.route("/")
 def index():
   return render_template("index.html")
@@ -13,6 +24,13 @@ def about():
 @app.route("/home")
 def home():
 	return render_template("home.html")
+
+@app.route("/signup")
+def signup():
+	form = SignupForm()
+	return render_template("signup.html", form=form)
+
+
 
 if __name__ == "__main__":
   app.run(debug=True)
